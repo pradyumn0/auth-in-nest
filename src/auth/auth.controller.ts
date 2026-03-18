56ca19010e16dbdd6625fdba72d54ae48e4fa7ee
Praduman Singh Jadon <pradyumnsingh453@gmail.com>
2026-03-18 23:34:05 +0530

    Auth ,User folder structure and declared morgan module for logs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService){}
   @Post('register')
   register() {
     return { message: 'User registered successfully' };
+  }
+
+  @Post('login')
+  login() {
+    return { message: 'User loged in successfully' };
   }
 }
